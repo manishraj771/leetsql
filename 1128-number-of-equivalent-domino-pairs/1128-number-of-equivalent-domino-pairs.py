a4ab00306440dba95742f783f07414b4c28a58a2
manish raj <bloodgamer752@gmail.com>
2025-05-04 16:55:22 +0530

    Time: 3 ms (100%), Space: 19.1 MB (60%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def numEquivDominoPairs(self, dominoes):
+        temp = [0] * 100
         count = 0
-        for i in range(len(dominoes)):
-            for j in range(i + 1, len(dominoes)):
-                a, b = dominoes[i]
-                c, d = dominoes[j]
-                if (a == c and b == d) or (a == d and b == c):
-                    count += 1
+        for a, b in dominoes:
+            key = a * 10 + b if a <= b else b * 10 + a
+            count += temp[key]
+            temp[key] += 1
         return count
+# class Solution(object):
+#     def numEquivDominoPairs(self, dominoes):
+#         count = 0
+#         for i in range(len(dominoes)):
+#             for j in range(i + 1, len(dominoes)):
+#                 a, b = dominoes[i]
+#                 c, d = dominoes[j]
+#                 if (a == c and b == d) or (a == d and b == c):
+#                     count += 1
+#         return count
